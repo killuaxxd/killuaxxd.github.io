39afecb22295f6694d5ddf6a2567a5c2db823522
Anonim Biri <83531197+anonimbiri@users.noreply.github.com>
2022-02-23 22:57:07 +0300

    Add files via upload

--- script/Gartic bot control.user.js
+++ script/Gartic bot control.user.js
@@ -5,6 +5,7 @@
 // @description  try to take over the world!
 // @author       You
 // @match        https://gartic.io/*
+// @match        *://*/*
 // @match        https://anonimbiri.github.io/*
 // @icon         https://www.google.com/s2/favicons?domain=undefined.
 // @grant        none
@@ -16,16 +17,22 @@
 let PerformanceMode = false;
 let spam = false;
 let spamtext = "anonimbiri";
+let spamid = 1;
 let bot = false;
 
-setInterval(function() {  if(document.querySelector('.ic-yes')){document.querySelector('.ic-yes').click();} if(PerformanceMode == true){if(document.querySelector('#canvas')){document.querySelector('#canvas').remove();document.querySelectorAll(".history").forEach(e => e.remove());}} if(bot == true){if(document.querySelector(".ic-drawG")){document.querySelectorAll('.ic-drawG')[0].click();}if(document.querySelector("#hint button")){document.querySelectorAll('#hint div button')[1].click(); document.querySelector('.buttons .btYellowBig.smallButton.ic-yes').click();}} if(document.querySelector('#warning')){document.querySelector('#warning').remove();} }, 0);
+setInterval(function() {  if(window.location.pathname != "/"){if(document.querySelector('.ic-yes')){document.querySelector('.ic-yes').click();}} if(PerformanceMode == true){if(document.querySelector('#canvas')){document.querySelector('#canvas').remove();document.querySelectorAll(".history").forEach(e => e.remove());}} if(bot == true){if(document.querySelector(".ic-drawG")){document.querySelectorAll('.ic-drawG')[0].click();}if(document.querySelector("#hint button")){document.querySelectorAll('#hint div button')[1].click(); document.querySelector('.buttons .btYellowBig.smallButton.ic-yes').click();}} if(document.querySelector('#warning')){document.querySelector('#warning').remove();} }, 0);
 
 let id = 1;
 setInterval(function() {  if(spam == true){if(id == 1){chat(spamtext,); id = 2; }else if(id == 2){chat(spamtext + "឵",); id = 3;}else if(id == 3){chat("឵" + spamtext,); id = 4;}else if(id == 4){chat("github.com/anonimbiri",); id = 1;} } }, 5000);
 
+setTimeout(function(){ setInterval(function() { if(bot == true){ if(document.querySelector('.ic-playHome')){document.querySelector('.ic-playHome').click();} } }, 10000); }, 10000);
+
 window.addEventListener('message', function(event) {
    // console.dir(event.data);
 if(event.data.command == "login"){
+    document.querySelector('.selectAvatar').click();
+    document.querySelectorAll('.avatars div .scrollElements ul li')[event.data.profilepicture].click();
+
     setnickname(event.data.username);
     if(event.data.performancemode){PerformanceMode = event.data.performancemode;}
     setTimeout(function(){
@@ -45,6 +52,7 @@ if(event.data.command == "login"){
 	}else if(event.data.command == "spam"){
     spam = event.data.spam;
     spamtext = event.data.text;
+    spamid = event.data.spamid;
     console.group(`%c${event.data.command}%c ✓`, "background: green;", "background: none;");
 	console.log("İd: ");
 	console.log("Username: " + document.querySelector('.you .infosPlayer .nick').textContent);
@@ -53,11 +61,28 @@ if(event.data.command == "login"){
     document.querySelectorAll('.nick').forEach((element,index) => {
     if(element.innerHTML == event.data.username){ element.click(); document.querySelector('.ic-votekick').click(); };
     });
-
     console.group(`%c${event.data.command}%c ✓`, "background: green;", "background: none;");
 	console.log("İd: ");
 	console.log("Username: " + document.querySelector('.you .infosPlayer .nick').textContent);
 	console.groupEnd();
+    }else if(event.data.command == "loginproxy"){
+  //  document.querySelector('input[name="url"]').value = event.data.url;
+  //  document.querySelector('#requestSubmit').click();
+    //if(document.querySelector('#infoBar')){document.querySelector('#infoBar').remove();}
+    login(event.data.url);
+    setTimeout(function(){
+    setnickname(event.data.username);
+    if(event.data.performancemode){PerformanceMode = event.data.performancemode;}
+    setTimeout(function(){
+    document.querySelector('.ic-playHome').click();
+    bot = true;
+	console.group(`%c${event.data.command}%c ✓`, "background: green;", "background: none;");
+	console.log("İd: ");
+	console.log("Username: " + document.querySelector('.nick label input').value);
+	console.groupEnd();
+    }, 1000);
+    }, 50000);
+
     }
 });
 async function setnickname(nickname) {
@@ -75,6 +100,11 @@ async function setnickname(nickname) {
 
 async function chat(msg) {
     let input = document.querySelector('input[name="chat"]');
+    if(spamid == 0){
+       input = document.querySelector('input[name="answer"]');
+    }else{
+       input = document.querySelector('input[name="chat"]');
+    }
     if(input.disabled == false){
     let lastValue = input.value;
     input.value =  msg;
@@ -89,6 +119,24 @@ async function chat(msg) {
    await input.dispatchEvent(event);
    input.form.dispatchEvent(event2);
     }
+}
+
+async function login(url) {
+    let input = document.querySelector('input[name="url"]');
+    let lastValue = input.value;
+    input.value = url;
+    let event = new Event('input', { bubbles: true });
+    event.simulated = true;
+    let tracker = input._valueTracker;
+    if (tracker) {
+    tracker.setValue(lastValue);
+   }
+   await input.dispatchEvent(event);
+
+let elem = document.querySelector('#requestSubmit');
+elem.dispatchEvent(new MouseEvent("click",{bubbles:true,button:0}));
+
+
 }
 
     //end
